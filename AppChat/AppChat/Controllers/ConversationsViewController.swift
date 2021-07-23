997f759612b94054539babc8ec439433fc807d1d
Tam Vu <tam.vm@aris-vn.com>
2021-07-23 17:11:24 +0700

    Part 16: Deleting Conversations +Add (https://www.youtube.com/watch?v=PTwORD5c7k0&list=PL5PR3UyfTWvdlk-Qi-dPtJmjTj-2YIMMf&index=18)

--- AppChat/AppChat/Controllers/ConversationsViewController.swift
+++ AppChat/AppChat/Controllers/ConversationsViewController.swift
@@ -45,6 +45,8 @@ class ConversationsViewController: UIViewController {
         return label
     }()
     
+    private var loginObserver: NSObjectProtocol?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .compose,
@@ -56,6 +58,16 @@ class ConversationsViewController: UIViewController {
         setupTableView()
         fetchConversations()
         startListeningForConversations()
+        
+        loginObserver = NotificationCenter.default.addObserver(forName: .didLogInNotification,
+                                                               object: nil,
+                                                               queue: .main,
+                                                               using: { [weak self]_ in
+                                                                guard let strongSelf = self else {
+                                                                    return
+                                                                }
+                                                                strongSelf.startListeningForConversations()
+                                                               })
     }
     
     private func startListeningForConversations() {
@@ -63,6 +75,10 @@ class ConversationsViewController: UIViewController {
             return
         }
 
+        if let observer = loginObserver {
+            NotificationCenter.default.removeObserver(observer)
+        }
+        
         print("Starting conversation fetch...")
 
         let safeEmail = DatabaseManager.safeEmail(emailAddress: email)
@@ -162,6 +178,33 @@ extension ConversationsViewController: UITableViewDelegate, UITableViewDataSourc
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 120
+    }
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            
+            // begin delete
+            
+            let conversationId = conversations[indexPath.row].id
+            
+            tableView.beginUpdates()
+            
+            DatabaseManager.shared.deleteConversation(conversationId: conversationId, completion: { [weak self] success in
+                if success {
+                    self?.conversations.remove(at: indexPath.row)
+                    
+                    tableView.deleteRows(at: [indexPath], with: .left)
+                }
+            })
+            
+            
+            
+            tableView.endUpdates()
+        }
     }
     
 }
